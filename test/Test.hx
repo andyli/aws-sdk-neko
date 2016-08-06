07d984a272cc7170a549a628b87e2c5d50726d24
Andy Li <andy@onthewings.net>
2016-08-06 12:17:08 +0800

    upload build artifacts to S3

--- test/Test.hx
+++ test/Test.hx
@@ -1,16 +1,17 @@
 import haxe.unit.*;
-using neko.Lib;
+
+import sys.io.*;
 
 import aws.*;
 import aws.s3.*;
 import aws.transfer.*;
 
 class Test extends TestCase {
-	public var AWS_DEFAULT_REGION = switch (Sys.getEnv("AWS_DEFAULT_REGION")) {
+	static public var AWS_DEFAULT_REGION = switch (Sys.getEnv("AWS_DEFAULT_REGION")) {
 		case null, "": throw 'AWS_DEFAULT_REGION is not set';
 		case v: v;
 	};
-	public var S3BUCKET_NAME = switch (Sys.getEnv("S3BUCKET_NAME")) {
+	static public var S3BUCKET_NAME = switch (Sys.getEnv("S3BUCKET_NAME")) {
 		case null, "": throw 'S3BUCKET_NAME is not set';
 		case v: v;
 	};
@@ -33,14 +34,66 @@ class Test extends TestCase {
 		assertEquals(null, r.getFailure());
 	}
 
+	static function uploadNdll():Void {
+		var fileName = sys.FileSystem.absolutePath("../bin/aws.ndll");
+		var sha = {
+			var p = new Process("git", ["rev-parse", "--short", "HEAD"]);
+			var out = p.stdout.readAll().toString();
+			p.close();
+			out;
+		}
+		var date = {
+			var p = new Process("git", ["show", "-s", "--format=%ct", "HEAD"]);
+			var out = p.stdout.readAll().toString();
+			p.close();
+			datetime.DateTime.fromTime(Std.parseFloat(out));
+		}
+		var dateString = date.format("%Y%m%d%H%M%S");
+		var s64 = switch (Sys.systemName()) {
+			case "Windows": "";
+			case _:
+				var p = new Process("file", [fileName]);
+				var out = p.stdout.readAll().toString();
+				p.close();
+				if (out.indexOf("x86_64") > -1)
+					"64";
+				else
+					"";
+		}
+		var platform = switch (Sys.systemName()) {
+			case "Windows": "Windows";
+			case "Mac": "Mac" + s64;
+			case "Linux": "Linux" + s64;
+			case systemName: throw 'unknown systemName: $systemName';
+		}
+		var keyName = 'artifacts/${dateString}_${sha}/ndll/${platform}/aws.ndll';
+
+		var client = new TransferClient(new S3Client(AWS_DEFAULT_REGION));
+		var bucketName = S3BUCKET_NAME;
+		var contentType = "application/octet-stream";
+		var r = client.uploadFile(fileName, bucketName, keyName, contentType);
+		while (!r.isDone()) {
+			Sys.sleep(0.5);
+		}
+		switch (r.getFailure()) {
+			case null:
+				Sys.println('uploaded ${fileName}');
+			case f:
+				throw f;
+		}
+	}
+
 	static function main():Void {
 		var runner = new TestRunner();
 		runner.add(new Test());
 		Aws.initAPI();
 		var succeeded = runner.run();
-		Aws.shutdownAPI();
 		if (!succeeded) {
+			Aws.shutdownAPI();
 			Sys.exit(1);
+		} else {
+			uploadNdll();
+			Aws.shutdownAPI();
 		}
 	}
 }
